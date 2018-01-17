b64f872963ab27e54ed496fe6da0c4774540a47f
Sam Elstob <samelstob@gmail.com>
2018-01-17 23:31:33 +0000

    Add code comments so that future self might understand something here

--- management-notes/algorithms/src/justify2.py
+++ management-notes/algorithms/src/justify2.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 import sys
 
+""" Number of calls to the badness function to demonstrate the time complexity of each solution """
 n_calls = 0
 
+"""
+Calculate a "badness" value for the fit of "words" for a given pagewidth
+
+This is the function whose value we are trying to minimise through DP
+
+@return "badness" of words for given pagewidth
+        sys.maxsize if the words exceed the pagewidth
+"""
 def badness(words, pagewidth):
     #print("badness: %s" % (words))
     global n_calls
@@ -14,11 +23,21 @@ def badness(words, pagewidth):
     if width > pagewidth:
         badness = sys.maxsize
     else:
+        # Cubed is from 6.006 problem definition - originally from LaTex
         badness = (pagewidth - width)**3
     return badness
 
 """
+Top-down recursive implementation of text justification using DP
+
+To calculate the minimum badness of the text - for each of the possible choices for where to split the first line, calculate the badness of the first line + the minimum badness of the suffix text (recursively) 
+
 Where to split lines so as to minimise total badness
+@param text - List of words
+@param pagewidth - Width of page to justify text
+@param use_memo - Whether to use memoization True/False
+@param memo - Dict for reusing solutions
+@return minimum badness
 """
 def justify(text, pagewidth, use_memo, memo):
     #print("justify: %s" % (text)) 
@@ -43,6 +62,15 @@ def justify(text, pagewidth, use_memo, memo):
             min_badness = memo[text_len]
         return min_badness
 
+"""
+Bottom-up DP solution to text justification
+
+Solve the sub-problems in order so as to reuse solutions without recursion
+
+To achieve this we work backwards through the text calculating the minimum badness of increasingly larger lists of words.
+
+The minimum badness is still calculated as the minimum of all the possible choices for the first line + the minimum badness of the suffix text
+"""
 def justify_bottom_up(text, pagewidth, use_memo, memo):
     text_len = len(text)
     min_badness = None
@@ -58,6 +86,7 @@ def justify_bottom_up(text, pagewidth, use_memo, memo):
         # Pre-condition: memo[i+1..text_len] is populated
         # Post-condition: min badness calculated for position i
         #print(memo)
+        # Try all the choices for the first line starting at i
         for j in range(text_len, i, -1):
             #print("i:%d, j:%d, text: %s" % (i, j, text[i:j]))
             this_badness = badness(text[i:j], pagewidth) + memo[j]
@@ -77,7 +106,9 @@ if __name__ == "__main__":
         #text = ["a", "b", "c", "d", "e", "f"]
         text = file.read().split()
         print(text)
+        # Test a range of subset of the given input text
         for i in range(1, 26):
+            # Test a range of pagewidths
             for pagewidth in range(4, 9):
                 print(text[0:i])
                 n_calls = 0
